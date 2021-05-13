a7dfa5994715477dc903d6be587c48019b4eea53
Yaroslav <serafim90@mail.ru>
2021-05-13 19:56:18 +0300

    main-search

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
+const Mentor = require('../models/mentor');
 
 const IndexRouter = require('express').Router();
-IndexRouter.get("/", (req, res) => {
-  res.render("index");
+IndexRouter.get("/", async (req, res) => {
+  const mentorAll = await Mentor.find();
+  const uniqueCompetencies = Array.from(new Set(mentorAll.map(x => x.competencies).flat()))
+  res.render("index", { uniqueCompetencies });
 });
 
 // IndexRouter.post('/', upload.single('avatar'), async (req, res) => {
