d95cb8f225c7a4ca67ecf2a2435cc1eb3bab6577
Shamil <indastr1es@gmail.com>
2021-05-14 12:02:55 +0300

    fuckingBugs

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,16 @@
 const filterForm = document.filterForm;
 const allMentors = document.querySelector("#allMentors");
-
 if (filterForm) {
   filterForm.addEventListener("submit", async (event) => {
     event.preventDefault();
-    allMentors.innerHTML = ""
+    // allMentors.innerHTML = ""
+    const checkCost = document.querySelector("#costSort");
+    const checkExperience = document.querySelector("#experianceSort");
     const select = document.querySelector("select")
-    if (select.options[select.selectedIndex] && select.options[select.selectedIndex].text !== 'Выберите компетенции') {
-      const skill = select.options[select.selectedIndex].text
+    const selectOption = select.options[select.selectedIndex]
+    const skill = select.options[select.selectedIndex].text
+    const defaultOptionText = 'Все наставники'
+    if (selectOption && skill !== defaultOptionText) {
       const responseSkill = await fetch("/mentor/showall", {
         method: "POST",
         headers: {
@@ -18,84 +21,190 @@ if (filterForm) {
         }),
       });
       const resultRes = await responseSkill.json();
-      
+      if (!checkCost.checked && !checkExperience.checked) {
       allMentors.innerHTML = "";
       resultRes
         .forEach((element) => {
-          allMentors.innerHTML += `<li>
-        <div> 
-          <p>Ментор: ${element.name}</p>
-          <p>Опыт работы: ${element.experience}</p>
-          <p>Стоимость занятий: ${element.payPerHour} руб/ч</p>
-          <p>${element.competencies}</p>
-        </div>
-      </li>`;
+          allMentors.innerHTML +=  `<div class="shadow p-3 mb-5 bg-white rounded">
+          <div class="card-content card mb-3 mx-3">
+            <div class="main-blocks-wrapper-mentors-content row g-0">
+              <div class="col-md-4 mentors-block-img">
+                <img class="imgStyle" height="150" width="300" src="${element.img}" />
+              </div>
+              <div class="col-md-8 wrapper-mentors-content">
+                <div class="card-body mentors-content">
+                  <h5 class="card-title">${element.name}</h5>
+                  <h2 class="card-title">${element.payPerHour}</h2>
+                  <h2 class="card-title">${element.experience}</h2>
+                  <p
+                    class="card-text"
+                  >blablablablablablablablablablablablablablablablablablablabla</p>
+                  <p class="card-text"><small
+                      class="text-muted"
+                    >${element.competencies}</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>`;
         });
+      }
+      if (checkCost.checked) {
+        allMentors.innerHTML = "";
+        resultRes
+          .sort((a, b) => a.payPerHour - b.payPerHour)
+          .forEach((element) => {
+            allMentors.innerHTML +=  `<div class="shadow p-3 mb-5 bg-white rounded">
+            <div class="card-content card mb-3 mx-3">
+              <div class="main-blocks-wrapper-mentors-content row g-0">
+                <div class="col-md-4 mentors-block-img">
+                  <img class="imgStyle" height="150" width="300" src="${element.img}" />
+                </div>
+                <div class="col-md-8 wrapper-mentors-content">
+                  <div class="card-body mentors-content">
+                    <h5 class="card-title">${element.name}</h5>
+                    <h2 class="card-title">${element.payPerHour}</h2>
+                    <h2 class="card-title">${element.experience}</h2>
+                    <p
+                      class="card-text"
+                    >blablablablablablablablablablablablablablablablablablablabla</p>
+                    <p class="card-text"><small
+                        class="text-muted"
+                      >${element.competencies}</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+          });
+      }
+      if (checkExperience.checked) {
+        allMentors.innerHTML = "";
+        resultRes
+          .sort((a, b) => a.experience - b.experience)
+          .forEach((element) => {
+            allMentors.innerHTML +=  `<div class="shadow p-3 mb-5 bg-white rounded">
+            <div class="card-content card mb-3 mx-3">
+              <div class="main-blocks-wrapper-mentors-content row g-0">
+                <div class="col-md-4 mentors-block-img">
+                  <img class="imgStyle" height="150" width="300" src="${element.img}" />
+                </div>
+                <div class="col-md-8 wrapper-mentors-content">
+                  <div class="card-body mentors-content">
+                    <h5 class="card-title">${element.name}</h5>
+                    <h2 class="card-title">${element.payPerHour}</h2>
+                    <h2 class="card-title">${element.experience}</h2>
+                    <p
+                      class="card-text"
+                    >blablablablablablablablablablablablablablablablablablablabla</p>
+                    <p class="card-text"><small
+                        class="text-muted"
+                      >${element.competencies}</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+          });
+      }
     }
-    // console.log(event.target);
-
-    const checkCost = document.querySelector("#costSort");
-    const checkExperience = document.querySelector("#experianceSort");
-    const response = await fetch("/mentor/showall", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        command: 1,
-      }),
-    });
-    const resultResponse = await response.json();
-    if (!checkCost.checked && !checkExperience.checked) {
-      const newResult = [...resultResponse];
-    }
-    if (checkExperience.checked && checkCost.checked) {
-      const newResult = [...resultResponse];
-      allMentors.innerHTML = "";
-      newResult
-        .sort((a, b) => a.experience - b.experience && a.payPerHour - b.payPerHour)
-        .forEach((element) => {
-          allMentors.innerHTML += `<li>
-        <div> 
-          <p>Ментор: ${element.name}</p>
-          <p>Опыт работы: ${element.experience}</p>
-          <p>Стоимость занятий: ${element.payPerHour} руб/ч</p>
-          <p>${element.competencies}</p>
-        </div>
-      </li>`;
-        });
-    }
-    if (checkCost.checked) {
-      const newResult = [...resultResponse];
-      allMentors.innerHTML = "";
-      newResult
-        .sort((a, b) => a.payPerHour - b.payPerHour)
-        .forEach((element) => {
-          allMentors.innerHTML += `<li>
-      <div> 
-        <p>Ментор: ${element.name}</p>
-        <p>Опыт работы: ${element.experience}</p>
-        <p>Стоимость занятий: ${element.payPerHour} руб/ч</p>
-        <p>${element.competencies}</p>
-      </div>
-    </li>`;
-        });
-    }
-    if (checkExperience.checked) {
-      const newResult = [...resultResponse];
-      allMentors.innerHTML = "";
-      newResult
-        .sort((a, b) => a.experience - b.experience)
-        .forEach((element) => {
-          allMentors.innerHTML += `<li>
-        <div> 
-          <p>Ментор: ${element.name}</p>
-          <p>Опыт работы: ${element.experience}</p>
-          <p>Стоимость занятий: ${element.payPerHour} руб/ч</p>
-          <p>${element.competencies}</p>
-        </div>
-      </li>`;
-        });
+    if (selectOption && skill === defaultOptionText) {
+      const response = await fetch("/mentor/showall", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          command: 1,
+        }),
+      });
+      const radioFilter = await response.json();
+      if (!checkCost.checked && !checkExperience.checked) {
+        allMentors.innerHTML = "";
+        radioFilter
+          .forEach((element) => {
+            allMentors.innerHTML +=  `<div class="shadow p-3 mb-5 bg-white rounded">
+            <div class="card-content card mb-3 mx-3">
+              <div class="main-blocks-wrapper-mentors-content row g-0">
+                <div class="col-md-4 mentors-block-img">
+                  <img class="imgStyle" height="150" width="300" src="${element.img}" />
+                </div>
+                <div class="col-md-8 wrapper-mentors-content">
+                  <div class="card-body mentors-content">
+                    <h5 class="card-title">${element.name}</h5>
+                    <h2 class="card-title">${element.payPerHour}</h2>
+                    <h2 class="card-title">${element.experience}</h2>
+                    <p
+                      class="card-text"
+                    >blablablablablablablablablablablablablablablablablablablabla</p>
+                    <p class="card-text"><small
+                        class="text-muted"
+                      >${element.competencies}</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+          });
+        }
+        if (checkCost.checked) {
+          allMentors.innerHTML = "";
+          radioFilter
+            .sort((a, b) => a.payPerHour - b.payPerHour)
+            .forEach((element) => {
+              allMentors.innerHTML +=  `<div class="shadow p-3 mb-5 bg-white rounded">
+              <div class="card-content card mb-3 mx-3">
+                <div class="main-blocks-wrapper-mentors-content row g-0">
+                  <div class="col-md-4 mentors-block-img">
+                    <img class="imgStyle" height="150" width="300" src="${element.img}" />
+                  </div>
+                  <div class="col-md-8 wrapper-mentors-content">
+                    <div class="card-body mentors-content">
+                      <h5 class="card-title">${element.name}</h5>
+                      <h2 class="card-title">${element.payPerHour}</h2>
+                      <h2 class="card-title">${element.experience}</h2>
+                      <p
+                        class="card-text"
+                      >blablablablablablablablablablablablablablablablablablablabla</p>
+                      <p class="card-text"><small
+                          class="text-muted"
+                        >${element.competencies}</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+            });
+        }
+        if (checkExperience.checked) {
+          allMentors.innerHTML = "";
+          radioFilter
+            .sort((a, b) => a.experience - b.experience)
+            .forEach((element) => {
+              allMentors.innerHTML +=  `<div class="shadow p-3 mb-5 bg-white rounded">
+              <div class="card-content card mb-3 mx-3">
+                <div class="main-blocks-wrapper-mentors-content row g-0">
+                  <div class="col-md-4 mentors-block-img">
+                    <img class="imgStyle" height="150" width="300" src="${element.img}" />
+                  </div>
+                  <div class="col-md-8 wrapper-mentors-content">
+                    <div class="card-body mentors-content">
+                      <h5 class="card-title">${element.name}</h5>
+                      <h2 class="card-title">${element.payPerHour}</h2>
+                      <h2 class="card-title">${element.experience}</h2>
+                      <p
+                        class="card-text"
+                      >blablablablablablablablablablablablablablablablablablablabla</p>
+                      <p class="card-text"><small
+                          class="text-muted"
+                        >${element.competencies}</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+            });
+        }
     }
   });
 }
